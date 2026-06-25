598b906bb8c20f070234dd50e49d223ddbc22772
Vaizer0 <137601862+Vaizer0@users.noreply.github.com>
2026-06-25 23:51:25 +0530

    Add files via upload

---
+++ coreui/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    alias(libs.plugins.noveldokusha.android.library)
+    alias(libs.plugins.noveldokusha.android.compose)
+    alias(libs.plugins.kotlin.parcelize)
+}
+
+android {
+    namespace = "my.noveldokusha.coreui"
+    androidResources {
+        resourcePrefix = ""
+    }
+}
+
+dependencies {
+    implementation(projects.strings)
+    implementation(projects.core)
+    implementation(projects.tooling.localDatabase)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    implementation(libs.compose.androidx.activity)
+    implementation(libs.compose.androidx.animation)
+    implementation(libs.compose.androidx.runtime.livedata)
+    implementation(libs.compose.androidx.lifecycle.viewmodel)
+    implementation(libs.compose.androidx.constraintlayout)
+    implementation(libs.compose.androidx.material.icons.extended)
+    implementation(libs.compose.material3.android)
+    implementation(libs.compose.landscapist.glide)
+    implementation(libs.compose.coil)
+    implementation(libs.compose.lazyColumnScrollbar)
+}
